27f3ff5de014df0e46c8f91e62e588d1fe8bdb21
Timo Nussbaum <timo.nussbaum@gmx.de>
2014-10-21 15:53:14 +0200

    Task: add report pdf as a new instance

--- Classes/DwarfPirate/TcPdf/Utility/ExtendedTcPdf.php
+++ Classes/DwarfPirate/TcPdf/Utility/ExtendedTcPdf.php
@@ -25,9 +25,27 @@ class ExtendedTcPdf {
 		$pdf = new \ExtendedTcPdf('P', 'mm', 'A4', true, 'UTF-8', false);
 		$pdf->setBackgroundImage($backgroundImage);
 		$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE, PDF_HEADER_STRING);
-		$pdf->SetMargins(PDF_MARGIN_LEFT, 50, 20);
+		$pdf->SetMargins(25, 50, 20);
 		$pdf->SetAutoPageBreak(TRUE, 40);
 		$pdf->setPrintFooter(false);
+		$pdf->AddImagePage();
+		return $pdf;
+	}
+
+	/**
+	 * @param $backgroundImage
+	 * @param int $topMargin
+	 * @param int $pageBreak
+	 * @return \ExtendedTcPdf
+	 */
+	public function createReportPdf($backgroundImage, $topMargin = 50, $pageBreak = 40) {
+		$this->initLibrary();
+		$pdf = new \ExtendedTcPdf('P', 'mm', 'A4', true, 'UTF-8', false);
+		$pdf->setBackgroundImage($backgroundImage);
+		$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE, PDF_HEADER_STRING);
+		$pdf->SetMargins(PDF_MARGIN_LEFT, $topMargin, 20);
+		$pdf->SetAutoPageBreak(TRUE, $pageBreak);
+		$pdf->setPrintFooter(true);
 		$pdf->AddImagePage();
 		return $pdf;
 	}
